23dd1b1124a56628abdc381bcc8f5d2f045602dc
guilherme27-03 <115494518+guilherme27-03@users.noreply.github.com>
2022-10-27 16:10:32 -0300

    atualização material escolar

---
+++ Material escolar/Apontador.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODELS
+{
+    public class Apontador
+    {
+        private string cor;
+        private string marca;
+        private string material;
+        private bool lamina;
+
+        public string Cor
+        {
+            get { return cor; }
+            set { cor = value; }
+        }
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+        public string Material
+        {
+            get { return material; }
+            set { material = value; }
+        }
+        public bool Lamina
+        {
+            get { return lamina; }
+            set { lamina = value; }
+        }
+        public Apontador(string _cor, string _marca, string _material)
+        {
+            Cor = _cor;
+            Marca = _marca;
+            Material = _material;
+
+
+
+        }
+        public void Apontar(bool _lamina)
+        {
+            lamina = true;
+        }
+        public void Apontar(string _text)
+        {
+            if (!Lamina)
+            {
+                Console.WriteLine("Impossivel de ser ultilizado sem lamina");
+                return;
+            }
+
+        }
+
+    }
+}
+
+
+
---
+++ Material escolar/Borracha.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Borracha
+    {
+        private string marca;
+        private int peso;
+        private string cor;
+        private int percentualdaborracha;
+
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+        public int Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+        public string Cor
+        {
+            get { return cor; }
+            set { cor = value; }
+        }
+        
+        public int Percentualdaborracha
+        {
+            get { return percentualdaborracha; }
+            set { percentualdaborracha = value; }
+        }
+
+        public Borracha(string _marca, string _peso, string _cor,int _percentualdaborracha)
+        {
+            marca = _marca;
+            peso = 30;
+            cor = _cor;
+            percentualdaborracha = 100;
+        }
+
+        private void Apagar()
+        {
+            Console.WriteLine("mensagem apagada");
+        }
+
+        private void Gastar()
+        {
+         percentualdaborracha -=10;
+        }
+
+    }
+}
+        
+          
+        
+
+
